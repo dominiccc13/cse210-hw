13e90e395d71ea690d61edf6d5842076ca4199f2
Your Name <you@example.com>
2023-10-11 19:23:57 -0600

    updated variables lesson 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,17 +8,17 @@ class Program
         // Fraction fractionObject = new Fraction();
         // fractionObject.setNumerator(1);
         // fractionObject.setDenominator(5);
-        var fractionObject = new Fraction(1);
-        System.Console.WriteLine(fractionObject.getAndReturnFraction());
-        System.Console.WriteLine(fractionObject.calculateAndReturnDecimal());
-        var fractionObject2 = new Fraction(5);
-        System.Console.WriteLine(fractionObject2.getAndReturnFraction());
-        System.Console.WriteLine(fractionObject2.calculateAndReturnDecimal());
-        var fractionObject3 = new Fraction(3,4);
-        System.Console.WriteLine(fractionObject3.getAndReturnFraction());
-        System.Console.WriteLine(fractionObject3.calculateAndReturnDecimal());
-        var fractionObject4 = new Fraction(1,3);
-        System.Console.WriteLine(fractionObject4.getAndReturnFraction());
-        System.Console.WriteLine(fractionObject4.calculateAndReturnDecimal());
+        var f = new Fraction(1);
+        System.Console.WriteLine(f.getAndReturnFraction());
+        System.Console.WriteLine(f.calculateAndReturnDecimal());
+        var f2 = new Fraction(5);
+        System.Console.WriteLine(f2.getAndReturnFraction());
+        System.Console.WriteLine(f2.calculateAndReturnDecimal());
+        var f3 = new Fraction(3,4);
+        System.Console.WriteLine(f3.getAndReturnFraction());
+        System.Console.WriteLine(f3.calculateAndReturnDecimal());
+        var f4 = new Fraction(1,3);
+        System.Console.WriteLine(f4.getAndReturnFraction());
+        System.Console.WriteLine(f4.calculateAndReturnDecimal());
     }
 }
